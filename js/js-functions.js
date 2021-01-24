bfce5d991d9c8027d05fb8dab641a9898665675a
Justas <justasvee@gmail.com>
2021-01-24 12:41:01 +0200

    Added localStorage functionality, income and expenses calculation and printing on click and window events

--- js/js-functions.js
+++ js/js-functions.js
@@ -10,7 +10,47 @@ const getSavedRecords = () => {
 }
 
 //Saves records to localStorage
-
 const saveRecords = () => {
     localStorage.setItem('records', JSON.stringify(records));
+}
+
+//Checks expense type
+const checkExpenseType = (type, amount) => {
+    if (type == 2) {
+        amount = parseFloat(amount);
+        return amount;
+    } else if (type == 1) {
+        amount = parseFloat(-amount);
+        return amount;
+    }
+}
+
+//Calculate income total
+const incomeTotal = () => {
+    const incomeNumber = document.querySelector('#income-number');
+    const recordsArray = getSavedRecords();
+    let count = 0;
+
+    recordsArray.forEach(record => {
+        if(record.type == 2) {
+            count += record.amount;
+        }
+    });
+    count = count.toFixed(2);
+    incomeNumber.textContent = count;
+}
+
+//Calculate expense total
+const expenseTotal = () => {
+    const expensesNumber = document.querySelector('#expenses-number');
+    const recordsArray = getSavedRecords();
+    let count = 0;
+
+    recordsArray.forEach(record => {
+        if(record.type == 1) {
+            count += record.amount;
+        }
+    });
+    count = count.toFixed(2);
+    expensesNumber.textContent = count;
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 //Gets records of expenses
 const records = getSavedRecords();
 
+window.addEventListener('load', (e) => {
+    if (records !== []) {
+        incomeTotal();
+        expenseTotal();
+    }
+}); 
+
 document.querySelector('#form').addEventListener('submit', (e) => {
     e.preventDefault();
-
     const type = document.querySelector('#type').value;
-    console.log(type);
+    const date = document.querySelector('#date').value;
+    const description = document.querySelector('#description').value.trim();
+    let amount = document.querySelector('#amount').value;
+    //Check expense type
+    amount = checkExpenseType(type, amount);
+
+
+    records.push({
+        type: type,
+        id: uuidv4(),
+        date: date,
+        description: description,
+        amount: amount
+    });
+    saveRecords();
+    incomeTotal();
+    expenseTotal();
 });
